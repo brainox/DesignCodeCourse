93320399cbc0d932b6e567465acd84d1459c6e30
brainox <macmartins081@gmail.com>
2021-07-20 17:54:50 +0100

    added sidebar view

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,24 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Decagon on 20/07/2021.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle("Courses")
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
