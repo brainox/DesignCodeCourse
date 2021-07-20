f222b0468480fcecea83c9ff78616c3ac13b7cd0
brainox <macmartins081@gmail.com>
2021-07-20 13:40:41 +0100

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Decagon on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
